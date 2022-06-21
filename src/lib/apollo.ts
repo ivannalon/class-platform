1a16d6ebec6a9634fcc8ea475bc6539ce04fa0e7
Ivan Nalon <ivannalon@gmail.com>
2022-06-21 19:14:22 -0300

    Configure apollo for API request

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,1 +1,6 @@
-import { ApolloClient } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: "https://api-sa-east-1.graphcms.com/v2/cl4o0tbnm02sl01z284ljdkt9/master",
+  cache: new InMemoryCache()
+})
